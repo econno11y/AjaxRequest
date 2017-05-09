a2ffc80e215a15432cf09d85731890979db19460
Erin Connolly <erin.a.connolly@gmail.com>
2017-05-09 11:39:17 -0600

    got it working

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$('.btn').on('click', function() {
+  $('.text').text('loading . . .');
+
+  $.ajax({
+    type:"GET",
+    url:"https://api.meetup.com/2/cities",
+    success: function(data) {
+      //remove loading icon
+      //create ul li with city names and zip
+      console.log(data.results)
+      var sContainer = document.getElementById("citylist");
+      for(var i = 0; i < data.results.length; i++) {
+        var city = data.results[i].city;
+        var zip = data.results[i].zip;
+        $(".text").remove();
+        $('#citylist').append("<li class='display'><strong>" + city + "</strong>"+ ": " + zip + "</li>");
+      }
+    },
+    dataType: 'jsonp',
+  });
+});
